2ae5690741422a2a1c7a26e706ce7f922a5f91ee
Richard W.M. Jones <rjones@redhat.com>
2016-08-06 15:47:23 +0100

    Mount some standard filesystems like /proc in the stage3 init.

--- init.sh
+++ init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -
 # Init script installed in stage3 disk image.
 
+# Root filesystem is mounted as ro, remount it as rw.
+mount -o remount,rw /
+
+# Mount standard filesystems.
+mount -t proc /proc /proc
+mount -t sysfs /sys /sys
+mount -t tmpfs -o "nosuid,size=20%,mode=0755" tmpfs /run
+mkdir -p /run/lock
+
+# XXX devtmpfs
+
 echo
 echo "Welcome to the Fedora/RISC-V stage3 disk image"
 echo "https://fedoraproject.org/wiki/Architectures/RISC-V"
