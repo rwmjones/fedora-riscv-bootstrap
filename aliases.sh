b49e75ca5060a5db6629f960aff9c6a6cbf71f0a
Richard W.M. Jones <rjones@redhat.com>
2016-09-05 14:43:41 +0100

    stage3: aliases: Install autoconf, automake for systemd build.

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,2 @@
 # Any useful aliases can go here.
-alias prep_systemd="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,expat,libtool,make,perl}-* /rpmbuild/RPMS/noarch/{intltool,perl}-*"
+alias prep_systemd="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,expat,libtool,make,perl}-* /rpmbuild/RPMS/noarch/{autoconf,automake,intltool,perl}-*"
