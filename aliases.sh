189c8e0fdffc79c52e7aaac52692127a567a2ec8
Richard W.M. Jones <rjones@redhat.com>
2016-09-05 13:24:39 +0100

    stage3: aliases: Install perl arch-ful packages too.

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,2 @@
 # Any useful aliases can go here.
-alias prep_systemd="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{make,bash,libtool}-* /rpmbuild/RPMS/noarch/{intltool,perl}-*"
+alias prep_systemd="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,libtool,make,perl}-* /rpmbuild/RPMS/noarch/{intltool,perl}-*"
