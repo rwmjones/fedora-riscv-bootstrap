7a7510bc91e54bc1dc65aaac3cde5fe6feae389d
Richard W.M. Jones <rjones@redhat.com>
2016-09-05 19:37:26 +0100

    Add all_rpms command which installs all RPMs.

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,3 @@
 # Any useful aliases can go here.
+alias all_rpms="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/*.rpm /rpmbuild/RPMS/noarch/*.rpm"
 alias prep_systemd="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,expat,libtool,make,perl}-* /rpmbuild/RPMS/noarch/{autoconf,automake,intltool,perl}-*"
