3247f2ae412174a4b5aca085575ddd2fa98b5508
Richard W.M. Jones <rjones@redhat.com>
2016-09-06 12:48:18 +0100

    stage3: aliases: Add 'missing_deps' command to display missing deps of RPM set.

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,11 @@
 alias all_rpms="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/*.rpm /rpmbuild/RPMS/noarch/*.rpm"
 alias prep_glibc="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,info,make}-*.rpm"
 alias prep_systemd="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,expat,libtool,make,perl}-* /rpmbuild/RPMS/noarch/{autoconf,automake,intltool,perl}-*"
+
+# Missing deps:
+#  missing_deps <some list of RPMs>
+# shows the missing dependencies from the set of RPMs.
+missing_deps ()
+{
+    rpm -Uvh --test "$@" |& grep "is needed by" | awk '{print $1}' | sort -u
+}
