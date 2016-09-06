81bc57ea616f859c65589fc64330c3b1c67cf88f
Richard W.M. Jones <rjones@redhat.com>
2016-09-06 12:29:30 +0100

    stage3: aliases: Add commands to prep glibc build.

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
 # Any useful aliases can go here.
 alias all_rpms="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/*.rpm /rpmbuild/RPMS/noarch/*.rpm"
+alias prep_glibc="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,info,make}-*.rpm"
 alias prep_systemd="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{bash,expat,libtool,make,perl}-* /rpmbuild/RPMS/noarch/{autoconf,automake,intltool,perl}-*"
