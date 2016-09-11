10e75311b4841e6ff173d151afd8e139be94df13
Richard W.M. Jones <rjones@redhat.com>
2016-09-11 21:06:28 +0100

    Fix install_tdnf alias.
    - Installs tdnf, rpm and all of their dependencies.
    - Rebuilds the RPM database.
    
    You still have to use 'tdnf --releasever 1.0 [cmd ...]'

--- aliases.sh
+++ aliases.sh
@@ -1,12 +1,4 @@
 # Any useful aliases can go here.
 alias all_rpms="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/*.rpm /rpmbuild/RPMS/noarch/*.rpm"
 
-alias install_tdnf="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{tdnf,librepo,hawkey,expat,libsolv,libcurl,gpgme,libassuan,openssl-libs,nss,nss-util,nspr,glib2,libgpg-error,pcre,libdb}-[0-9]*.rpm; ldconfig"
-
-# Missing deps:
-#  missing_deps <some list of RPMs>
-# shows the missing dependencies from the set of RPMs.
-missing_deps ()
-{
-    rpm -Uvh --test "$@" |& grep "is needed by" | awk '{print $1}' | sort -u
-}
+alias install_tdnf="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{tdnf,librepo,hawkey,expat,libsolv,libcurl,gpgme,libassuan,openssl-libs,nss,nss-util,nspr,glib2,libgpg-error,pcre,libdb,rpm,rpm-libs,popt,libcap,libacl,lua,bzip2-libs,libattr,nss-softokn,nss-softokn-freebl,sqlite-libs}-[0-9]*.rpm; ldconfig; rpm --initdb"
