6f42060084b2223a4a998cc3ad849e27a1c1e56f
Richard W.M. Jones <rjones@redhat.com>
2016-09-11 21:46:12 +0100

    Create /var/cache/tdnf/local for caching of local.repo.

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
 # Any useful aliases can go here.
 alias all_rpms="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/*.rpm /rpmbuild/RPMS/noarch/*.rpm"
 
-alias install_tdnf="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{tdnf,librepo,hawkey,expat,libsolv,libcurl,gpgme,libassuan,openssl-libs,nss,nss-util,nspr,glib2,libgpg-error,pcre,libdb,rpm,rpm-libs,popt,libcap,libacl,lua,bzip2-libs,libattr,nss-softokn,nss-softokn-freebl,sqlite-libs}-[0-9]*.rpm; ldconfig; rpm --initdb"
+alias install_tdnf="rpm --nodeps -Uvh /rpmbuild/RPMS/riscv64/{tdnf,librepo,hawkey,expat,libsolv,libcurl,gpgme,libassuan,openssl-libs,nss,nss-util,nspr,glib2,libgpg-error,pcre,libdb,rpm,rpm-libs,popt,libcap,libacl,lua,bzip2-libs,libattr,nss-softokn,nss-softokn-freebl,sqlite-libs}-[0-9]*.rpm; ldconfig; rpm --initdb; mkdir -p /var/cache/tdnf/local"
